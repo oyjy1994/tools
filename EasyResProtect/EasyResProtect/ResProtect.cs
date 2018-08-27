687da0c95b092d142479b5767bf2158128d554cd
oyjy1994 <ouyangjianyu@163.com>
2018-08-27 14:48:17 +0800

    add rule

--- EasyResProtect/EasyResProtect/ResProtect.cs
+++ EasyResProtect/EasyResProtect/ResProtect.cs
@@ -6,9 +6,16 @@ using System.Text;
 
 namespace EasyResProtect
 {
+    /// <summary>
+    /// MAX_ENCRYPT_LEN之前的数据进行全Aes加密,之后的数据进行规制混合。
+    /// 待测试检验。
+    /// </summary>
     class ResProtect
     {
+        const int MAX_ENCRYPT_LEN = 512; //最大Aes加密长度
         const int PASSWORD_LENGTH = 32;
+        const int CONFUSE_OFFSET = 16; //混淆间隔
+        const int BLOCK_SIZE = 16;
         static byte[] IV = new byte[] { 0xa2, 0xb4, 0xc6, 0xd8, 0xe0, 0xfB, 0xCD, 0xEF, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
         static AesCryptoServiceProvider aesProvider;
 
@@ -54,13 +61,62 @@ namespace EasyResProtect
         static byte[] ExecuteEncrypt(byte[] buffer, string key)
         {
             //这里后续附加混淆规则
-            return Encrypt(buffer, key);
+            int bufferLen = buffer.Length;
+            if (bufferLen > MAX_ENCRYPT_LEN)
+            {
+                //ECBMode:(明文长度/16 + 1) * 16 = 密文长度
+                int retLen = bufferLen - MAX_ENCRYPT_LEN + (MAX_ENCRYPT_LEN / BLOCK_SIZE + 1) * BLOCK_SIZE;
+                byte[] encryBuffer = new byte[MAX_ENCRYPT_LEN];
+                byte[] retBuffer = new byte[retLen];
+                Array.Copy(buffer, encryBuffer, MAX_ENCRYPT_LEN);
+                byte[] beEncryBuffer = Encrypt(encryBuffer, key);
+                beEncryBuffer.CopyTo(retBuffer, 0);
+                Array.Copy(buffer, MAX_ENCRYPT_LEN, retBuffer, beEncryBuffer.Length, bufferLen - MAX_ENCRYPT_LEN);
+
+                for (int i = beEncryBuffer.Length, j = 0; i < retBuffer.Length; i += CONFUSE_OFFSET)
+                {
+                    retBuffer[i] += buffer[j]; j += 2;
+                    if (j >= MAX_ENCRYPT_LEN)
+                        j = 0;
+                }
+
+                return retBuffer;
+            }
+            else
+            {
+                return Encrypt(buffer, key);
+            }
         }
         
         static byte[] ExecuteDecrypt(byte[] buffer, string key)
         {
             //这里后续附加混淆规则
-            return Decrypt(buffer, key);
+            int bufferLen = buffer.Length;
+            if (bufferLen > MAX_ENCRYPT_LEN)
+            {
+                //ECBMode:(明文长度/16 + 1) * 16 = 密文长度
+                int decryLen = (MAX_ENCRYPT_LEN / BLOCK_SIZE + 1) * BLOCK_SIZE;
+                byte[] decryBuffer = new byte[decryLen];
+                Array.Copy(buffer, decryBuffer, decryLen);
+                byte[] beDecryBuffer = Encrypt(decryBuffer, key);
+                int retLen = bufferLen - decryLen + beDecryBuffer.Length;
+                byte[] retBuffer = new byte[retLen];
+                beDecryBuffer.CopyTo(retBuffer, 0);
+                Array.Copy(buffer, decryLen, retBuffer, beDecryBuffer.Length, retBuffer.Length - beDecryBuffer.Length);
+
+                for (int i = beDecryBuffer.Length, j = 0; i < retLen; i += CONFUSE_OFFSET)
+                {
+                    retBuffer[i] -= beDecryBuffer[j]; j += 2;
+                    if (j >= beDecryBuffer.Length)
+                        j = 0;
+                }
+
+                return retBuffer;
+            }
+            else
+            {
+                return Decrypt(buffer, key);
+            }
         }
 
         static byte[] Encrypt(byte[] buffer, string key)
